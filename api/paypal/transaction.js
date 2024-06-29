d602f42b116cc7179a3e6a09703c8e4511aae828
arturmauricioss <arturmauricioss@outlook.com>
2024-06-28 23:00:56 -0300

    Add files via upload

--- api/paypal/transaction.js
+++ api/paypal/transaction.js
@@ -1,6 +1,8 @@
+// /api/paypal/transaction.js
+
 const express = require('express');
 const paypal = require('paypal-rest-sdk');
-const QRCode = require('qrcode'); // Biblioteca para gerar QR Codes
+
 const app = express();
 app.use(express.json());
 
@@ -11,17 +13,18 @@ paypal.configure({
   client_secret: process.env.PAYPAL_SECRET,
 });
 
-// Endpoint para criar uma transação do PayPal
 app.post('/api/paypal/transaction', (req, res) => {
   const { size, color, price } = req.body;
+
+  // Configuração do pagamento
   const create_payment_json = {
     intent: 'sale',
     payer: {
       payment_method: 'paypal',
     },
     redirect_urls: {
-      return_url: 'http://yourwebsite.com/success',
-      cancel_url: 'http://yourwebsite.com/cancel',
+      return_url: 'https://arturmauricioss.github.io/pagarme/public/sucesso.html',
+      cancel_url: 'https://arturmauricioss.github.io/pagarme/public/falha.html',
     },
     transactions: [{
       item_list: {
@@ -41,6 +44,7 @@ app.post('/api/paypal/transaction', (req, res) => {
     }],
   };
 
+  // Criar o pagamento
   paypal.payment.create(create_payment_json, (error, payment) => {
     if (error) {
       res.status(500).json({ error });
@@ -52,35 +56,6 @@ app.post('/api/paypal/transaction', (req, res) => {
       }
     }
   });
-});
-
-// Endpoint para gerar QR Code Pix
-const pixKey = '761e2934-2b53-4059-a31d-62eaad45168e';
-
-app.get('/api/pix/qr-code', (req, res) => {
-    const amount = req.query.amount || 100.00; // Valor do pagamento
-    const description = req.query.description || 'Pagamento de Blusa';
-
-    // Gerar o QR Code Pix
-    const pixPayload = `00020126360014BR.GOV.BCB.PIX0114${pixKey}02020${amount.toFixed(2)}5204000053039865405${amount.toFixed(2)}5802BR5925MERCHANT_NAME6009BRASIL61080540900062070503${description}`;
-    QRCode.toDataURL(pixPayload, (err, qrCodeData) => {
-        if (err) {
-            return res.status(500).send('Erro ao gerar QR Code');
-        }
-        res.json({ qrCodeData });
-    });
-});
-
-// Endpoint de sucesso do PayPal
-app.get('/success', (req, res) => {
-  // Processar a confirmação do pagamento do PayPal aqui
-  res.send('Pagamento realizado com sucesso');
-});
-
-// Endpoint de cancelamento do PayPal
-app.get('/cancel', (req, res) => {
-  // Processar o cancelamento do pagamento do PayPal aqui
-  res.send('Pagamento cancelado');
 });
 
 app.listen(3000, () => {
